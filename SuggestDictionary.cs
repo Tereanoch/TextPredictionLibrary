38cffb96c3c39f452b934565d59ee1676913b0f9
Tereanoch <varnitsah94@gmail.com>
2017-06-19 20:44:51 -0700

    Text Prediction Library

---
+++ SuggestDictionary.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text_Prediction
+{
+    /// <summary>
+    /// This class handles the bigram pairs
+    /// </summary>
+    class SuggestDictionary
+    {
+        public TernarySearchTree st;
+        public Dictionary<String, String> ht = new Dictionary<String, String>();
+
+        /// <summary>
+        /// Get next word suggestion on the basis of bigram pairs
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public List<String> getHashSuggestions(String str)
+        {
+            List<String> sugg = new List<string>();
+            String[] data = null;
+            st = null;
+            st = new TernarySearchTree();
+            Dictionary<String, int> dict = new Dictionary<string, int>();
+            if (ht.ContainsKey(str))
+            {
+                if (ht[str] == "")
+                    return sugg;
+                String[] suggestions = ht[str].Split('&');
+                List<String> list = new List<string>(suggestions);
+                list = list.GetRange(1, list.Count - 2);
+
+                foreach (String i in list)
+                {
+                    data = i.Split(';');
+                    sugg.Add(data[0]);
+                    if(!dict.ContainsKey(data[0]))
+                    dict.Add(data[0], int.Parse(data[1]));
+                    st.insert(data[0], int.Parse(data[1]));
+                }
+                list = new List<string>();
+                var variable = from pair in dict
+                               orderby pair.Value descending
+                               select pair;
+
+
+                // Add results to list
+                foreach (KeyValuePair<string, int> pair in variable)
+                {
+                    list.Add(pair.Key);
+                }
+                return list;
+            }
+            return new List<String>();
+        }
+
+        /// <summary>
+        /// give a reduced autocomplete list based on next words.
+        /// </summary>
+        /// <param name="prefix">Incomplete word</param>
+        /// <returns>A list of possible suggestions</returns>
+        public List<string> getAutoSuggestSuggestions(string prefix)
+        {
+            List<string> sugg = new List<string>();
+            sugg = st.getAutoCompleteSuggestions(prefix);
+            return sugg;
+        }
+    }
+}
---
+++ TernarySearchTree.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text_Prediction
+{
+    /// <summary>
+    /// This class contains methods to handle the Ternary Search Tree Data Structure, to store the data dictionary efficiently. 
+    /// </summary>
+    class TernarySearchTree
+    {
+        /// <summary>
+        /// A root node for the tree.
+        /// </summary>
+        public TSTNode root = null;
+        Dictionary<String, int> dict = new Dictionary<String, int>();
+        String pre;
+
+        /// <summary>
+        /// A method to initialize the root node.Call this method before inserting the dictionary into the tree.
+        /// </summary>
+        /// <param name="c">Inserting a character which is in the .middle of the language character set is recommended.</param>
+        public void createRoot(char c)
+        {
+            root = new TSTNode(c);
+        }
+
+        /// <summary>
+        /// Recursive search function. Not to be called explicitly
+        /// </summary>
+        /// <param name="node">TST node</param>
+        /// <param name="word">A simple string</param>
+        /// <param name="pos">pointer position in the word</param>
+        /// <param name="frequency">frequency of the word</param>
+        /// <returns></returns>
+        private TSTNode insert(TSTNode node, string word, int pos, int frequency)
+        {
+
+            if (node == null)
+            {
+                node = new TSTNode(word[pos]);
+            }
+
+            if (word[pos] < node.Ch)
+            {
+                node.left = insert(node.left, word, pos, frequency);
+            }
+            else if (word[pos] > node.Ch)
+            {
+                node.right = insert(node.right, word, pos, frequency);
+            }
+            else
+            {
+                if (pos + 1 < word.Length)
+                {
+                    node.middle = insert(node.middle, word, pos + 1, frequency);
+                }
+                else
+                {
+                    node.wordEnd = true;
+                    node.frequency = frequency;
+                }
+            }
+            return node;
+        }
+
+        /// <summary>
+        /// insert words into the tree
+        /// </summary>
+        /// <param name="word">A normal word</param>
+        /// <param name="frequency">The pre-decided frequency count in the dictionary</param>
+        public void insert(string word, int frequency)
+        {
+            root = insert(root, word, 0, frequency);
+        }
+
+        private TSTNode traverse(TSTNode node, string prefix, int pos)
+        {
+            if (node != null)
+            {
+                if (prefix[pos] < node.Ch)
+                {
+                    return traverse(node.left, prefix, pos);
+                }
+                else if (prefix[pos] > node.Ch)
+                {
+
+                    return traverse(node.right, prefix, pos);
+                }
+                else
+                {
+                    if (pos + 1 == prefix.Length)
+                    {
+                        return node;
+                    }
+                    else
+                    {
+                        return traverse(node.middle, prefix, pos + 1);
+
+                    }
+                }
+            }
+            return node;
+        }
+
+        public TSTNode traverse(string prefix)
+        {
+            TSTNode n = traverse(root, prefix, 0);
+            return n;
+        }
+
+        /// <summary>
+        /// recursive search funciton. Not to be called explicitly
+        /// </summary>
+        /// <param name="node">TST node</param>
+        /// <param name="str">A simple String</param>
+        private void search(TSTNode node, String str)
+        {
+            if (node == null)
+                return;
+            else
+            {
+                search(node.left, str);
+                str = str + node.Ch;
+                if (node.wordEnd)
+                {
+                    dict.Add(pre + str, node.frequency);
+                }
+
+                search(node.middle, str);
+                str = str.Substring(0, str.Length - 1);
+                search(node.right, str);
+            }
+        }
+
+        /// <summary>
+        /// A function to search the possible words based on the prefix entry.
+        /// </summary>
+        /// <param name="prefix">An incomplete or complete word</param>
+        /// <returns>A list of words</returns>
+        private List<String> search(String prefix)
+        {
+            pre = prefix;
+            List<String> list = new List<string>();
+            TSTNode start = traverse(prefix);
+            if (start != null)
+            {
+                if (start.wordEnd == true)
+                {
+                    dict.Add(prefix, start.frequency);
+                }
+
+                search(start.middle, "");
+
+                // Order by values.
+                // Use LINQ to specify sorting by value.
+                var suggestions = from pair in dict
+                                  orderby pair.Value descending
+                                  select pair;
+
+                
+                // Add results to list
+                foreach (KeyValuePair<string, int> pair in suggestions)
+                {
+                    list.Add(pair.Key);
+                }
+
+                return list;
+            }
+            return list;
+        }
+
+        /// <summary>
+        /// Returns the specified no. of suggestions
+        /// </summary>
+        /// <param name="prefix">Initial matching prefix String</param>
+        /// <returns>A list with suggestions ranked highest to lowest based on frequency</returns>
+        public List<String> getAutoCompleteSuggestions(String prefix)
+        {
+            dict.Clear();
+            List<String> list = new List<String>();
+            list = search(prefix);            
+            return list;
+        }
+    }
+
+    /// <summary>
+    /// Ternary Search Tree node
+    /// </summary>
+    class TSTNode
+    {
+        private char ch;
+        public TSTNode left, right, middle;
+        public int frequency;
+        public bool wordEnd;
+
+        public TSTNode(char c)
+        {
+            ch = c;
+            left = null;
+            middle = null;
+            right = null;
+            wordEnd = false;
+        }
+
+        public char Ch
+        {
+            get { return ch; }
+            set { ch = Ch; }
+        }
+    }
+
+}
+
